82ba950bfe6ba4e583cb86d0db1836444fbc98bf
Candido <candido.neto@ucsal.edu.br>
2021-11-25 10:40:25 -0300

    Activity de login e register agora rem botões funcionais

--- app/src/main/java/com/example/finderapp/Cadastro.java
+++ app/src/main/java/com/example/finderapp/Cadastro.java
@@ -2,11 +2,18 @@ package com.example.finderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.finderapp.R;
+import com.example.finderapp.models.User;
 
 public class Cadastro extends AppCompatActivity {
+    private EditText nome, senha, email, confirmSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,38 @@ public class Cadastro extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_cadastro);
+        nome = (EditText) findViewById(R.id.Name);
+        senha = (EditText) findViewById(R.id.Senha);
+        email = (EditText) findViewById(R.id.Email);
+        confirmSenha = (EditText) findViewById(R.id.confirmSenha);
+    }
+
+    public void cadastrar(View view) {
+        if (senha.getText().toString().equals(confirmSenha.getText().toString())) {
+            User u = pegarUsuario();
+            if (u.getEmail().equals("") && u.getName().equals("") && u.getPassword().equals("")){
+                Toast.makeText(getApplicationContext(),"Alguns campos estão vazios",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                //TODO adicionar no banco de dados
+                //TODO logar no sistema
+                //TODO trocar activity para tela principal
+            }
+        } else {
+            Toast.makeText(getApplicationContext(),"Senhas não batem",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void trocarPraLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    private User pegarUsuario() {
+        User u = new User(nome.getText().toString(),
+                          senha.getText().toString(),
+                          email.getText().toString());
+        return u;
     }
 }
--- app/src/main/java/com/example/finderapp/Login.java
+++ app/src/main/java/com/example/finderapp/Login.java
@@ -2,13 +2,48 @@ package com.example.finderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.finderapp.models.User;
 
 public class Login extends AppCompatActivity {
+    private EditText senha, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        senha = (EditText) findViewById(R.id.Senha);
+        email = (EditText) findViewById(R.id.Email);
+    }
+
+    public void login(View view) {
+        //TODO trocar a variavel "estaNoBanco" por um metodo que confirme se o
+        // usuario esta no banco de dados usando "pegarUsuario()" como parametro
+        boolean estaNoBanco = false;
+        pegarUsuario();
+        if (estaNoBanco) {
+            //TODO logar no sistema
+            //TODO trocar activity para tela principal
+        } else {
+            Toast.makeText(getApplicationContext(),"Email ou Senha Incorretos",
+                           Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void trocarPraCadastro(View view) {
+        Intent intent = new Intent(this, Cadastro.class);
+        startActivity(intent);
+    }
+
+    private User pegarUsuario(){
+        User u = new User("Place Holder",
+                senha.getText().toString(),
+                email.getText().toString());
+        return u;
     }
 }
