d0743b27b2da57482876dc87c142197e8c690cd9
Lara Ferreira <lara.ferrer001@gmail.com>
2021-11-24 21:06:34 -0300

    add adapter class

--- app/src/main/java/com/example/finderapp/FindOrderActivity.java
+++ app/src/main/java/com/example/finderapp/FindOrderActivity.java
@@ -3,12 +3,18 @@ package com.example.finderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.finderapp.models.Event;
+import com.example.finderapp.models.RastreioAdapter;
 import com.example.finderapp.services.OrderServices;
+
+import java.util.Collections;
+import java.util.List;
 
 public class FindOrderActivity extends AppCompatActivity {
     private OrderServices correiosServices;
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.correiosServices = new OrderServices();
@@ -16,5 +22,18 @@ public class FindOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_order);
 
         //correiosServices.getPackageList("LX002249507BR");
+
+
+
+    }
+    private void adicionarEncomenda(){
+
+    }
+    private void persquisarEncomenda(){
+        carregarLista();
+    }
+    private void carregarLista() {
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(new RastreioAdapter((List<Event>) new Event(),this));
     }
 }
---
+++ app/src/main/java/com/example/finderapp/models/RastreioAdapter.java
@@ -0,0 +1,53 @@
+package com.example.finderapp.models;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.finderapp.R;
+
+import java.util.List;
+
+public class RastreioAdapter extends BaseAdapter {
+    private final List<Event> events;
+    private final Activity act;
+
+    public RastreioAdapter(List<Event> events, Activity act) {
+        this.events = events;
+        this.act = act;
+    }
+
+    @Override
+    public int getCount() {
+        return events.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return events.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = act.getLayoutInflater().inflate(R.layout.activit_status_list, parent, false);
+        Event event = events.get(position);
+
+        TextView title = view.findViewById(R.id.text1);
+        TextView date = view.findViewById(R.id.text2);
+        TextView status = view.findViewById(R.id.text3);
+
+        date.setText(event.getDate() + " - " + event.getHour());
+        status.setText(event.getStatus() + " - " + event.getLocal());
+
+        return view;
+    }
+
+}
